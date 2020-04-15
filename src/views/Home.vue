d5124266a542d86564c04f9247d26f6b4ecf8417
wayne <wodutoit@gmail.com>
2020-04-15 17:21:55 +0200

    basic stepper added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,18 @@
   <div class="home">
     <img width="100%" alt="Vue logo" src="../assets/amazingrace.jpg">
     <h1>Welcome to your Amazing Lockdown Race</h1>
+    <stepper></stepper>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue';
+import stepper from '@/components/stepper.vue';
 
 export default {
   name: 'Home',
   components: {
+    stepper,
   },
 };
 </script>
