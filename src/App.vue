b1fe845225fdaadb51adce4e4185d9e693b597d1
wayne <wodutoit@gmail.com>
2020-04-15 15:57:17 +0200

    setup project again

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+      </div>
+
+      <v-spacer></v-spacer>
+      <v-toolbar-title >
+        <router-link active-class="link-active" to="/">Home</router-link>
+      </v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+<style scoped>
+.link-active{
+  color: aliceblue;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img width="100%" alt="Vue logo" src="../assets/amazingrace.jpg">
+    <h1>Welcome to your Amazing Lockdown Race</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'Home',
+  components: {
+  },
+};
+</script>
