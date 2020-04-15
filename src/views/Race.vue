160891e835fb84acdc1d5efec121c74d1224ab2e
wayne <wodutoit@gmail.com>
2020-04-15 14:12:30 +0200

    setup basic page

---
+++ src/views/Race.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img width="100%" alt="logo" src="../assets/amazingrace.jpg">
+    <h1>Welcome to your Amazing LockDown Race </h1>
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    //HelloWorld
+  }
+}
+</script>
