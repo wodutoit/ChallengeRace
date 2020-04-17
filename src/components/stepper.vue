51fff5df85227bce3a14c8b66852416ec236e824
wayne <wodutoit@gmail.com>
2020-04-17 17:54:01 +0200

    started adding tasks

--- src/components/stepper.vue
+++ src/components/stepper.vue
@@ -1,14 +1,47 @@
 <template>
   <v-stepper v-model="e6" vertical>
     <v-stepper-step :complete="e6 > 1" step="1">
-        Select an app: {{ data.legs.length }}
-      <small>Summarize if needed</small>
+        {{ clues[0].title }}
+      <small>Let's get this party started!</small>
     </v-stepper-step>
 
     <v-stepper-content step="1">
-      <v-card color="grey lighten-1" class="mb-12" height="200px"></v-card>
-      <v-btn color="primary" @click="e6 = 2">Continue</v-btn>
-      <v-btn text>Cancel</v-btn>
+      <v-card class="mx-auto" outlined>
+        <v-list-item three-line>
+          <v-list-item-content>
+            <v-list-item-title class="headline mb-1">{{ clues[0].riddle }}</v-list-item-title>
+            <v-list-item-subtitle  @click="hint0=true">
+              Do you need some Help?
+              <v-icon small color="darken-2">
+              mdi-information-outline
+            </v-icon>
+            </v-list-item-subtitle>
+            <v-card-text class="text--primary">
+              <p v-if="hint0">{{ clues[0].hint }}</p>
+              <br>
+              <v-text-field
+                  v-model="answerInput[0]"
+                  solo
+                  label="Answer:"
+                  clearable
+                ></v-text-field>
+            </v-card-text>
+          </v-list-item-content>
+          <v-list-item-avatar
+              tile
+              size="80"
+            >
+            <v-icon v-if="answerInput[0] === clues[0].answer" large color="green darken-2">
+              mdi-thumb-up
+            </v-icon>
+            </v-list-item-avatar>
+        </v-list-item>
+        <v-card-actions>
+          <v-btn color="primary" @click="e6 = 2" :disabled="answerInput[0] !== clues[0].answer">
+              Continue
+          </v-btn>
+        </v-card-actions>
+      </v-card>
     </v-stepper-content>
 
     <v-stepper-step :complete="e6 > 2" step="2">Configure analytics for this app</v-stepper-step>
@@ -43,12 +76,18 @@ export default {
   data() {
     return {
       e6: 1,
+      answerInput: [],
+      hint0: false,
     };
   },
   computed: {
-    data() {
-      return this.$store.state.dataStore;
+    clues() {
+      return this.$store.state.dataStore.clues;
     },
+  },
+  methods: {
+  },
+  watch: {
   },
 };
 </script>
--- src/store/dataStore.js
+++ src/store/dataStore.js
@@ -1,57 +1,76 @@
 
 const dataStore = {
-  legs: [
+  clues: [
     {
       id: 1,
-      title: 'Poolsilvania',
-      clues: [
-        {
-          id: 1,
-          title: 'Start',
-          riddle: 'Go by foot to the library, "quote from a well known book"',
-          hint: 'book# - page# - line#',
-          answer: '',
-        },
-        {
-          id: 2,
-          title: 'Detour - a task both team members must complete',
-          riddle: '',
-          hint: '',
-          answer: '',
-        },
-      ],
+      title: 'Start',
+      riddle: 'Go by foot to the library, "quote from a well known book"',
+      hint: 'book# - page# - line#',
+      answer: '1234',
+      files: [],
     },
     {
       id: 2,
-      title: 'Tramplestine',
-      clues: [
-        {
-          id: 1,
-          riddle: '',
-          answer: '',
-        },
-        {
-          id: 2,
-          riddle: '',
-          answer: '',
-        },
-      ],
+      title: 'Road Block - Who can feel the rythm, one or both team members may perform the road block the first to complete the task earns a clue for their team.',
+      riddle: 'You need to perform this traditional South African mine workers dance. - The gumboot dance.',
+      hint: '',
+      answer: 'dance monkey',
+      files: {
+        learn: '../assets/GumbootDance.mp4',
+        image: '',
+      },
     },
     {
       id: 3,
-      title: 'Race to the Finish',
-      clues: [
-        {
-          id: 1,
-          riddle: '',
-          answer: '',
-        },
-        {
-          id: 2,
-          riddle: '',
-          answer: '',
-        },
-      ],
+      title: 'Road Block - Time to farm',
+      riddle: 'You must make your way by foot the outdoor gym. There you will find the water containers required by a local farmer. Take your water container and find the 4 filling stations to fill your container before returning to the farmer. Finally stack the water containers on top of each other to access you next clue.',
+      hint: '',
+      answer: 'Farmer Carry',
+      files: {
+        learn: '',
+        image: '',
+      },
+    },
+    {
+      id: 3,
+      title: 'Road Block - Artisitic Skills',
+      riddle: 'Look at the picture below then go to the drawing station to create your own version of the image. You may only return once to view the picture. You have 5 minutes to complete the challenge. If you fail you will have to complete 30 seconds of skipping without stopping or tripping over the skipping rope.',
+      hint: 'draw picture',
+      answer: '',
+      files: {
+        learn: '',
+        image: 'zulu-wall-painting.jpg',
+      },
+    },
+    {
+      id: 4,
+      title: 'Road Block - Can you build it?',
+      riddle: 'Build a tower out of plasic cups in under 30 seconds that is at least 60cm high.',
+      hint: 'draw picture',
+      answer: '',
+      files: {
+        learn: '',
+        image: '',
+      },
+    },
+    {
+      id: 4,
+      title: 'Detour - a task both team members must complete',
+      riddle: 'Make your way to the pool of destiny. You must work together to cross from the square to the triangle. There is no going around!',
+      hint: '',
+      answer: '',
+      files: {
+        learn: '',
+        image: '../assets/change-we-can-relieve-in-1.png',
+      },
+    },
+    {
+      id: 5,
+      title: 'Home Run',
+      riddle: 'Make your way to the pitstop and final stop of the race. Mountian of headcracking pullups.',
+      hint: '',
+      answer: '',
+      files: [],
     },
   ],
 };
