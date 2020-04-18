5df5044049fbd074d98c2ec5bb14fe7c272e5309
wayne <wodutoit@gmail.com>
2020-04-18 15:09:29 +0200

    removed video player

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import Vue from 'vue';
-import vuePlayer from '@algoz098/vue-player';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
@@ -7,8 +6,6 @@ import store from './store';
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
-
-Vue.component(vuePlayer);
 
 new Vue({
   router,
