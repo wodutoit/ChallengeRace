f0f540d0b60f07d2404fdb76474fafea4239f0d3
wayne <wodutoit@gmail.com>
2020-04-20 16:45:12 +0200

    added background to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,11 @@
   <div class="home">
     <img width="100%" alt="Vue logo" src="/assets/amazingrace.jpg">
     <h1>Welcome to your Amazing Lockdown Race</h1>
-    <pre><h3>Roadblock</h3> - A road block is a challenge that one team member (mummy) must perform.
-    Team mate can also try if they would like.</pre>
-    <pre><h3>Detour</h3> - A detour is a challenge that must be completed as a team.</pre>
+    <pre><h3 class="error">Roadblock</h3>
+    A road block is a challenge that one team member (mummy)
+    must perform. Team mate can also try if they would like.</pre>
+    <pre><h3 class="warning">Detour</h3>
+    A detour is a challenge that must be completed as a team.</pre>
     <stepper></stepper>
   </div>
 </template>
